8ba7e0e625372642f367d39282d328254e739324
zoynul <zoynulabedin@Zoynuls-MacBook-Air.local>
2024-02-05 17:31:46 +0600

    update

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://cchat-o3qd.onrender.com:10000",
     credentials: true,
   })
 );
